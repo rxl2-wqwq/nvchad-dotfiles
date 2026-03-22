ca39ffbf595bcde7f7780bc805c409d9fa33cb13
rxl2-wqwq <muzal5192@gmail.com>
2026-03-22 18:38:52 +0700

    update mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,7 +7,7 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
-map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+map({ "n" }, "<C-s>", "<cmd> w <cr>")
 
 vim.keymap.set("n", "<leader>ra", function()
   require "nvchad.lsp.renamer"()
